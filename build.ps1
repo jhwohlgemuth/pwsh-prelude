b0cdfb6d03a8f4acefcff2750fd2e4ff43849076
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-10-15 15:37:25 -0500

    dev: Remove Verbose switch from build.ps1

--- build.ps1
+++ build.ps1
@@ -97,7 +97,7 @@ function Invoke-Publish
     "INVALID`n" | Write-Host -ForegroundColor Red
   }
   "==> Publishing module..." | Write-Host -ForegroundColor Cyan -NoNewline
-  Publish-Module -Path (Get-Location) -NuGetApiKey $Env:NUGET_API_KEY -Verbose
+  Publish-Module -Path (Get-Location) -NuGetApiKey $Env:NUGET_API_KEY
   "DONE`n" | Write-Host -ForegroundColor Green
 }
 if ($Lint) {
