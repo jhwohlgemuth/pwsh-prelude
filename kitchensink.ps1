449de07ea30b8d4d3805e8d8491414c212f5b94b
Jason Wohlgemuth <jhwohlgemuth@users.noreply.github.com>
2020-10-16 16:38:52 -0500

    Improve consistent use of space and indent

--- kitchensink.ps1
+++ kitchensink.ps1
@@ -5,25 +5,27 @@ $Indent = 4
 $Color = "Green"
 
 Write-Title "   Kitchen Sink   " -SubText "#allthethings" -Yellow
+
 $Space
+
 $Fullname = Invoke-Input "Full Name?" -Indent $Indent
 $Username = Invoke-Input "Username?" -MaxLength 10 -Indent $Indent
 $Age = Invoke-Input "Age?" -Number -Indent $Indent
 $Pass = Invoke-Input "Password?" -Secret -Indent $Indent
 $FavoriteSaiyajin = Invoke-Input "Favorite Saiya-jin?" -Autocomplete -Indent $Indent -Choices `
 @(
-    'Goku'
-    'Gohan'
-    'Goten'
-    'Vegeta'
-    'Trunks'
+  'Goku'
+  'Gohan'
+  'Goten'
+  'Vegeta'
+  'Trunks'
 )
 
 'Favorite number?' | Write-Label -Indent $Indent -NewLine
 $FavoriteNumber = "one","two","three" | Invoke-Menu -SingleSelect -Indent $Indent
 
 'Known mathematicians?' | Write-Label -Indent $Indent -NewLine
-$choice = "Godel","Gauss","Cantor" | Invoke-Menu -MultiSelect -Indent $Indent
+$Choice = "Godel","Gauss","Cantor" | Invoke-Menu -MultiSelect -Indent $Indent
 
 "{{#red Red}}, {{#white White}}, or {{#blue Blue}}?" | Write-Label -Indent $Indent -NewLine
 $FavoriteColor = "red","white","blue" | Invoke-Menu -Indent $Indent
@@ -37,7 +39,7 @@ $Age | Write-Label -Indent $Indent -Color $Color -NewLine
 $Pass | Write-Label -Indent $Indent -Color $Color -NewLine
 $FavoriteSaiyajin | Write-Label -Indent $Indent -Color $Color -NewLine
 $FavoriteNumber | Write-Label -Indent $Indent -Color $Color -NewLine
-(Join-StringsWithGrammar $choice) | Write-Label -Indent $Indent -Color $Color -NewLine
+(Join-StringsWithGrammar $Choice) | Write-Label -Indent $Indent -Color $Color -NewLine
 $FavoriteColor | Write-Label -Indent $Indent -Color $Color -NewLine
 
 $Space
@@ -45,11 +47,11 @@ $Space
 "Show bar charts?" | Write-Label -NewLine
 $Choice = "yes","no" | Invoke-Menu
 if ($Choice -eq "yes") {
-    "Bar Charts" | Write-Title -Blue
-    Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart
-    Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -Alternate
-    Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -ShowValues
-    Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -ShowValues -Alternate
-    Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -ShowValues -WithColor
-    Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -ShowValues -WithColor -Alternate
+  "Bar Charts" | Write-Title -Blue
+  Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart
+  Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -Alternate
+  Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -ShowValues
+  Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -ShowValues -Alternate
+  Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -ShowValues -WithColor
+  Get-ChildItem -File | Invoke-Reduce -FileInfo | Show-BarChart -ShowValues -WithColor -Alternate
 }
