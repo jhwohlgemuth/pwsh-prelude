5cb0502f81c3675ed549806fa3b89fc1ce31f04d
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-10-19 16:36:58 -0500

    docs: Add example to Invoke-RunApplication

--- src/application.ps1
+++ src/application.ps1
@@ -8,6 +8,17 @@ function Invoke-RunApplication {
     Code to execute during every application loop, executed when ShouldContinue returns True.
     .PARAMETER BeforeNext
     Code to execute at the end of each application loop. It should be used to update the return of ShouldContinue.
+    .EXAMPLE
+    $Init = {
+      $Global:Continue = 'yes'
+      'Setting up application...' | Write-Color -Green
+    }
+    $Loop = {
+      Clear-Host
+      'Doing something super important...' | Write-Color -Gray
+      Start-Sleep 5
+    }
+    Invoke-RunApplication $Init $Loop
     #>
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'Continue')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', 'Global:Continue')]
