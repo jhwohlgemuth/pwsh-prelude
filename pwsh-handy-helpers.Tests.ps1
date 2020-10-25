14a9800694ee5957c26725e27ed614407b22f3de
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-10-25 16:30:58 -0500

    feat: Initial addition of web request wrappers

--- pwsh-handy-helpers.Tests.ps1
+++ pwsh-handy-helpers.Tests.ps1
@@ -6,7 +6,7 @@ Import-Module "${PSScriptRoot}\pwsh-handy-helpers.psm1" -Force
 Describe 'Handy Helpers Module' {
     Context 'meta validation' {
         It 'should import exports' {
-            (Get-Module -Name pwsh-handy-helpers).ExportedFunctions.Count | Should -Be 63
+            (Get-Module -Name pwsh-handy-helpers).ExportedFunctions.Count | Should -Be 66
         }
         It 'should import aliases' {
             (Get-Module -Name pwsh-handy-helpers).ExportedAliases.Count | Should -Be 26
--- src/data.ps1
+++ src/data.ps1
@@ -1,4 +1,12 @@
-﻿function Format-MoneyValue {
+﻿function ConvertTo-Iso8601 {
+  [CmdletBinding()]
+  Param(
+    [Parameter(Position=0, ValueFromPipeline=$true)]
+    [String] $Value
+  )
+  $Value | Get-Date -UFormat '+%Y-%m-%dT%H:%M:%S.000Z'
+}
+function Format-MoneyValue {
   <#
   .SYNOPSIS
   Helper function to create human-readable money (USD) values as strings.
@@ -280,4 +288,55 @@ function Invoke-Reduce {
   End {
     $Result
   }
+}
+function Invoke-WebRequestWithBasicAuth {
+  <#
+  .SYNOPSIS
+  Invoke-WebRequest wrapper that makes it easier to use basic authentication
+  .PARAMETER TwoFactorAuthentication
+  Name of API that requires 2FA. Use 'none' when 2FA is not required.
+  Possible values:
+  - 'Github'
+  - 'none' [Default]
+  .EXAMPLE
+  $Uri = 'https://api.github.com/notifications' 
+  $Query = @{ 'per_page' = 100; page = 1}
+  $Request = Invoke-WebRequestWithBasicAuth $Username $Token -Uri $Uri -Query $Query
+  $Request.Content | ConvertFrom-Json | Format-Table -AutoSize
+  #>
+  [CmdletBinding()]
+  Param(
+    [Parameter(Position=0)]
+    [String] $Username,
+    [Parameter(Position=1)]
+    [String] $Token,
+    [Uri] $Uri,
+    [PSObject] $Query,
+    [PSObject] $Data,
+    [String] $TwoFactorAuthentication = 'none'
+  )
+  $Credential = [Convert]::ToBase64String([System.Text.Encoding]::Ascii.GetBytes("${Username}:${Token}"))
+  $Headers = @{
+    Authorization = "Basic $Credential"
+  }
+  switch ($TwoFactorAuthentication.ToLower()) {
+    'github' {
+      $Code = '2FA Code:' | Invoke-Input -Number
+      $Headers.Accept = 'application/vnd.github.v3+json'
+      $Headers['x-github-otp'] = $Code
+    }
+    Default {
+      # Do nothing
+    }
+  }
+  $Parameters = @{
+    Uri = $Uri
+    Headers = $Headers
+  }
+  Invoke-WebRequest @Parameters
+}
+function Invoke-WebRequestWithOAuth {
+  [CmdletBinding()]
+  Param()
+  
 }
