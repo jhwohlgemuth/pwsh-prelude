83d2b1250be44707202a3658af1711d33feeb2dc
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-11-06 18:52:55 -0600

    chore: Clear lint issues

--- classes/ApplicationState.ps1
+++ classes/ApplicationState.ps1
@@ -1,16 +1,16 @@
-if ('ApplicationState' -as [Type]) {
+﻿if ('ApplicationState' -as [Type]) {
   return
 }
 Add-Type -TypeDefinition @"
     using System;
-    
+
     public class ApplicationState {
-    
+
         public string Id;
         public string Name;
         public bool Continue;
         public object Data;
-        
+
         public ApplicationState() {
             this.Id = Guid.NewGuid().ToString();
             this.Name = "Application Name";
