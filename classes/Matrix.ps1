249d6e73e6f0eeff16484e8ff365d8efbc307589
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-11-09 16:29:04 -0600

    feat: Add Matrix Unit and Transpose methods, create test framework for Matrix class

--- classes/Matrix.ps1
+++ classes/Matrix.ps1
@@ -1,31 +1,54 @@
-if ('Matrix' -as [Type]) {
+# Need to parameterize class with "id" in order to re-load class during testing
+$Id = if ($Env:ProjectName -eq 'pwsh-prelude') { 'Test' } else { '' }
+if ("Matrix${Id}" -as [Type]) {
   return
 }
 Add-Type -TypeDefinition @"
     using System;
 
-    public class Matrix {
+    public class Matrix${Id} {
 
         public int[] Order;
         public double[][] Values;
 
-        public Matrix(int n) {
+        public Matrix${Id}(int n) {
             this.Order = new int[2];
             this.Order[0] = n;
             this.Order[1] = n;
-            this.Values = Matrix.Create(n,n);
+            this.Values = Matrix${Id}.Create(n,n);
         }
-        public Matrix(int m, int n) {
+        public Matrix${Id}(int m, int n) {
             this.Order = new int[2];
             this.Order[0] = m;
             this.Order[1] = n;
-            this.Values = Matrix.Create(m,n);
+            this.Values = Matrix${Id}.Create(m,n);
         }
         public static double[][] Create(int m, int n) {
             double[][] result = new double[m][];
             for (var i = 0; i < m; ++i)
                 result[i] = new double[n];
             return result;
+        }
+        public static Matrix${Id} Unit(int size) {
+            var temp = new Matrix${Id}(size,size);
+            for (var i = 0; i < temp.Order[0]; ++i)
+                temp.Values[i][i] = 1;
+            return temp;
+        }
+        public static Matrix${Id} Transpose(Matrix${Id} a) {
+            var m = a.Order[0];
+            var n = a.Order[1];
+            var temp = new Matrix${Id}(n,m);
+            for (var row = 0; row < m; ++row)
+                for (var col = 0; col < n; ++col)
+                    temp.Values[row][col] = a.Values[col][row];
+            return temp;
+        }
+        public Matrix${Id} Add(Matrix${Id} a, Matrix${Id} b) {
+            return a;
+        }
+        public Matrix${Id} Multiply(Matrix${Id} a, Matrix${Id} b) {
+            return a;
         }
     }
 "@
--- tests/classes.Tests.ps1
+++ tests/classes.Tests.ps1
@@ -3,15 +3,46 @@
 Describe 'Matrix Class' {
     It 'can create an NxN multi-dimensional array' {
         $N = 5
-        $Matrix = [Matrix]::New($N)
+        $Matrix = [MatrixTest]::New($N)
         $Matrix.Values.Count | Should -Be $N
         $Matrix.Values[0].Count | Should -Be $N
     }
     It 'can create an MxN multi-dimensional array' {
         $M = 8
         $N = 6
-        $Matrix = [Matrix]::New($M,$N)
+        $Matrix = [MatrixTest]::New($M,$N)
         $Matrix.Values.Count | Should -Be $M
         $Matrix.Values[0].Count | Should -Be $N
+    }
+    It 'can create unit matrices' {
+        $Unit = [MatrixTest]::Unit(2)
+        $Unit.Order | Should -Be 2,2
+        $Unit.Values[0] | Should -Be 1,0
+        $Unit.Values[1] | Should -Be 0,1
+        $Unit = [MatrixTest]::Unit(4)
+        $Unit.Order | Should -Be 4,4
+        $Unit.Values[0] | Should -Be 1,0,0,0
+        $Unit.Values[1] | Should -Be 0,1,0,0
+        $Unit.Values[2] | Should -Be 0,0,1,0
+        $Unit.Values[3] | Should -Be 0,0,0,1
+    }
+    It 'can transpose matrices' {
+        $Matrix = [MatrixTest]::New(3)
+        $Matrix.Values[0][0] = 1
+        $Matrix.Values[0][1] = 2
+        $Matrix.Values[0][2] = 3
+        $Matrix.Values[1][0] = 4
+        $Matrix.Values[1][1] = 5
+        $Matrix.Values[1][2] = 6
+        $Matrix.Values[2][0] = 7
+        $Matrix.Values[2][1] = 8
+        $Matrix.Values[2][2] = 9
+        $Matrix.Values[0] | Should -Be 1,2,3
+        $Matrix.Values[1] | Should -Be 4,5,6
+        $Matrix.Values[2] | Should -Be 7,8,9
+        $Transposed = [MatrixTest]::Transpose($Matrix)
+        $Transposed.Values[0] | Should -Be 1,4,7
+        $Transposed.Values[1] | Should -Be 2,5,8
+        $Transposed.Values[2] | Should -Be 3,6,9
     }
 }
