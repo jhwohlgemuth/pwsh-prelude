32ebe886193afc560a4af99af2424552593aecc5
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-11-22 18:13:10 -0600

    fix(ci): Replace variables with string values for prelude class

--- src/classes/Prelude.ps1
+++ src/classes/Prelude.ps1
@@ -1,4 +1,3 @@
-$ClassName = 'Prelude'
 # Need to parameterize class with "id" in order to re-load class during local testing
 $Id = if ($Env:ProjectName -eq 'pwsh-prelude' -and $Env:BuildSystem -eq 'Unknown') { 'Test' } else { '' }
 $TypeDefinition = @"
@@ -18,12 +17,12 @@ $TypeDefinition = @"
       }
   }
 "@
-if ("${ClassName}${Id}" -as [Type]) {
+if ("Prelude${Id}" -as [Type]) {
   return
 } else {
   Add-Type -TypeDefinition $TypeDefinition
   if ($Env:BuildSystem -eq 'Travis CI') {
     $Accelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
-    $Accelerators::Add("${ClassName}${Id}", $ClassName)
+    $Accelerators::Add('PreludeTest', 'Prelude')
   }
 }
