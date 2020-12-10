061361bf7c3f8f53a9a9e95523cd1403f028979c
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-12-09 21:42:04 -0600

    fix: use script root for matrix source code path again again

--- src/matrix.ps1
+++ src/matrix.ps1
@@ -1,7 +1,7 @@
 ﻿if ('Matrix' -as [Type]) {
   # Do nothing
 } else {
-  Add-Type -TypeDefinition (Get-Content -Path (Join-Path $PSScriptRoot 'csharp/Matrix/Matrix.cs'))
+  Add-Type -TypeDefinition (Get-Content -Path (Join-Path $PSScriptRoot 'csharp/Matrix/Matrix.cs').ToString())
 }
 
 function New-Matrix {
