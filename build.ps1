638b1dacd7fa511932bf01ee4cbcbb6891ac0bce
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2021-01-04 16:32:42 -0600

    dev: Combine coverage, refactor build, update appveyor config

--- build.ps1
+++ build.ps1
@@ -21,6 +21,9 @@ Exclude running tests (Describe or It) with certain tags
 .EXAMPLE
 .\build.ps1 -CI -Test -Tags 'Remote' -Exclude 'LinuxOnly' -WithCoverage
 
+.EXAMPLE
+.\build.ps1 -Test -WithCoverage -Skip dotnet
+
 #>
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '')]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('AdvancedFunctionHelpContent', '')]
@@ -31,7 +34,9 @@ Param(
   [Switch] $WithCoverage,
   [Switch] $ShowCoverageReport,
   [Switch] $CI,
-  [Switch] $SkipChecks,
+  [ValidateSet('dotnet', 'powershell')]
+  [String[]] $Skip,
+  [Switch] $BuildOnly,
   [String] $Filter,
   [String[]] $Tags,
   [String] $Exclude = '',
@@ -54,69 +59,62 @@ function Invoke-Lint {
     ReportSummary = $True
     Recurse = $True
   }
-  Invoke-ScriptAnalyzer @Parameters
-  '' | Write-Output
+  Invoke-ScriptAnalyzer @Parameters -Confirm:$False
+  "`n" | Write-Output
 }
 function Invoke-Test {
   [CmdletBinding()]
   Param()
-  $Files = (Get-ChildItem (Join-Path $PSScriptRoot $SourceDirectory) -Recurse -Include '*.ps1').FullName
-  $Configuration = [PesterConfiguration]@{
-    Run = @{
-      PassThru = $True
+  if (-not ($Skip -contains 'dotnet')) {
+    $Message = if ($CI) { "==> Executing C# tests on $Env:BuildSystem" } else { '==> Executing C# tests' }
+    $Message | Write-Output
+    $ProjectPath =  "$PSScriptRoot/src/cs/Tests/Tests.csproj"
+    if ($WithCoverage) {
+      dotnet test $ProjectPath /p:CollectCoverage=true /p:CoverletOutput=coverage.xml /p:CoverletOutputFormat=opencover
+    } else {
+      dotnet test $ProjectPath --logger:'console;verbosity=detailed'
     }
-    Filter = @{
-      ExcludeTag = $Exclude
-    }
-    Debug = @{
-      ShowNavigationMarkers = $True
-      WriteVSCodeMarker = $True
-    }
+    "`n`n" | Write-Output
   }
-  if ($Filter) {
-    $Configuration.Filter.FullName = $Filter
-  } elseif ($Tags) {
-    $Configuration.Filter.Tag = $Tags
-  }
-  if ($WithCoverage) {
-    $Configuration.CodeCoverage = @{
-      Enabled = $True
-      Path = $Files
+  if (-not ($Skip -contains 'powershell')) {
+    $Files = (Get-ChildItem (Join-Path $PSScriptRoot $SourceDirectory) -Recurse -Include '*.ps1').FullName
+    $Configuration = [PesterConfiguration]@{
+      Run = @{ PassThru = $True }
+      Filter = @{ ExcludeTag = $Exclude }
+      Debug = @{ ShowNavigationMarkers = $True; WriteVSCodeMarker = $True }
     }
-    $Configuration.TestResult = @{
-      Enabled = $False
+    if ($Filter) {
+      $Configuration.Filter.FullName = $Filter
+    } elseif ($Tags) {
+      $Configuration.Filter.Tag = $Tags
     }
-  }
-  if ($CI) {
-    Set-BuildEnvironment -VariableNamePrefix '' -Force
-    "==> Executing tests on $Env:BuildSystem" | Write-Output
-  } else {
-    '==> Executing tests' | Write-Output
-  }
-  $Result = Invoke-Pester -Configuration $Configuration
-  if ($Result.FailedCount -gt 0) {
-    $FailedMessage = "==> FAILED - $($Result.FailedCount) test(s) failed"
-    throw $FailedMessage
-  } else {
-    "`nSUCCESS`n" | Write-Output
+    if ($WithCoverage) {
+      $Configuration.CodeCoverage = @{ Enabled = $True; Path = $Files }
+      $Configuration.TestResult = @{ Enabled = $False }
+    }
+    if ($CI) {
+      Set-BuildEnvironment -VariableNamePrefix '' -Force
+      "==> Executing PowerShell tests on $Env:BuildSystem" | Write-Output
+    } else {
+      '==> Executing PowerShell tests' | Write-Output
+    }
+    $Result = Invoke-Pester -Configuration $Configuration
+    if ($Result.FailedCount -gt 0) {
+      $FailedMessage = "==> FAILED - $($Result.FailedCount) PowerShell test(s) failed"
+      throw $FailedMessage
+    } else {
+      "`nPowerShell SUCCESS`n" | Write-Output
+    }
   }
 }
 function Invoke-Publish {
   [CmdletBinding()]
   Param()
   Set-BuildEnvironment -VariableNamePrefix '' -Force
-  "${Prefix}Validating module data..." | Write-Output
-  if (Test-ModuleManifest -Path $Env:PSModuleManifest) {
-    "${Prefix}==> SUCCESS" | Write-Output
-  } else {
-    "${Prefix}==> FAIL" | Write-Output
-  }
-  "${Prefix}Validating Nuget API Key..." | Write-Output
-  if ((Write-Output $Env:NUGET_API_KEY).Length -eq 46) {
-    "${Prefix}==> SUCCESS" | Write-Output
-  } else {
-    "${Prefix}==> FAIL" | Write-Output
-  }
+  $ValidateManifest = if (Test-ModuleManifest -Path $Env:PSModuleManifest) { 'Manifest is Valid' } else { 'Manifest is NOT Valid' }
+  $ValidateApiKey = if ((Write-Output $Env:NUGET_API_KEY).Length -eq 46) { 'API Key is Valid' } else { 'API Key is NOT Valid' }
+  "${Prefix}==> $ValidateManifest" | Write-Output
+  "${Prefix}==> $ValidateApiKey" | Write-Output
   $Increment = if ($Major) {
     'Major'
   } elseif ($Minor) {
@@ -136,24 +134,24 @@ function Invoke-Publish {
     "${Prefix}==> DONE" | Write-Output
   }
 }
-if ($Lint) {
+if ($Lint -and -not $BuildOnly) {
   Invoke-Lint
 }
-if ($Test -and -not $SkipChecks) {
+if ($Test -and -not $BuildOnly) {
   Invoke-Test
 }
-if ($ShowCoverageReport -and (Test-Path (Join-Path (Get-Location) 'coverage.xml'))) {
+if ($ShowCoverageReport) {
   $ReportTypes = 'Html;HtmlSummary;HtmlChart'
   if ($Test -and $WithCoverage) {
     $SourceDirs = "$SourceDirectory"
-    reportgenerator.exe -reports:coverage.xml -targetdir:coverage -sourcedirs:$SourceDirs -historydir:.history -reporttypes:$ReportTypes
+    reportgenerator.exe -reports:'**/coverage.xml' -targetdir:coverage -sourcedirs:$SourceDirs -historydir:.history -reporttypes:$ReportTypes
   } else {
-    reportgenerator.exe -reports:coverage.xml -targetdir:coverage -sourcedirs:$SourceDirectory -reporttypes:$ReportTypes
+    reportgenerator.exe -reports:'**/coverage.xml' -targetdir:coverage -sourcedirs:$SourceDirs -reporttypes:$ReportTypes
   }
-  Invoke-Item '.\coverage\index.htm'
+  Invoke-Item './coverage/index.htm'
 }
 if (-not $Lint -and -not $Test -and -not $ShowCoverageReport) {
-  if (-not $SkipChecks) {
+  if (-not $BuildOnly -and -not $DryRun) {
     Invoke-Lint
     Invoke-Test
   }
