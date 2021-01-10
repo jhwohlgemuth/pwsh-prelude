60e1b239e016383317943e81943d3ac20395ca07
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2021-01-09 19:17:13 -0600

    fix(dev): Remove install test for setup script

--- Invoke-Setup.ps1
+++ Invoke-Setup.ps1
@@ -1,13 +1,6 @@
 ﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute('AdvancedFunctionHelpContent', '')]
 [CmdletBinding()]
 Param()
-function Test-Installed {
-  $Name = $Args[0]
-  Get-Module -ListAvailable -Name $Name
-}
 'PSScriptAnalyzer', 'BuildHelpers', 'Pester' | ForEach-Object {
-  $Installed = Test-Installed $_
-  if (-not $Installed) {
-    Install-Module -Name $_ -Scope CurrentUser -Force
-  }
+  Install-Module -Name $_ -Scope CurrentUser -Force
 }
