e17af17052e8d093c9b56ef1d0bf62e2523b730b
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2021-03-19 19:29:42 -0500

    feat: Add [Complex] type accelerator, refactor loading all accelerators; bump version

--- Prelude/Prelude.psm1
+++ Prelude/Prelude.psm1
@@ -16,35 +16,44 @@
 <#
 Import link libraries and create type accelarators
 #>
+function Add-TypeData {
+    Param(
+        [String] $Name
+    )
+    $Path = Join-Path $PSScriptRoot "bin/${Name}.dll"
+    if (Test-Path $Path) {
+        Add-Type -Path $Path
+    } else {
+        "==> Failed to load ${Name} type accelerators" | Write-Warning
+    }
+}
+$Accelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+if (-not 'Complex' -as [Type]) {
+    $Accelerators::Add('Complex', 'System.Numerics.Complex')
+}
 if (-not ('Prelude.Matrix' -as [Type])) {
-    Add-Type -Path (Join-Path $PSScriptRoot 'bin/Matrix.dll')
-    $Accelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+    Add-TypeData -Name 'Matrix'
     $Accelerators::Add('Matrix', 'Prelude.Matrix')
 }
 if (-not ('Prelude.Geodetic' -as [Type])) {
-    Add-Type -Path (Join-Path $PSScriptRoot 'bin/Geodetic.dll')
-    $Accelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+    Add-TypeData -Name 'Geodetic'
     $Accelerators::Add('Coordinate', 'Prelude.Geodetic.Coordinate')
     $Accelerators::Add('Datum', 'Prelude.Geodetic.Datum')
 }
 if (-not ('Prelude.Node' -as [Type])) {
-    Add-Type -Path (Join-Path $PSScriptRoot 'bin/Node.dll')
-    $Accelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+    Add-TypeData -Name 'Node'
     $Accelerators::Add('Node', 'Prelude.Node')
 }
 if (-not ('Prelude.Edge' -as [Type])) {
-    Add-Type -Path (Join-Path $PSScriptRoot 'bin/Edge.dll')
-    $Accelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+    Add-TypeData -Name 'Edge'
     $Accelerators::Add('Edge', 'Prelude.Edge')
 }
 if (-not ('Prelude.DirectedEdge' -as [Type])) {
-    Add-Type -Path (Join-Path $PSScriptRoot 'bin/DirectedEdge.dll')
-    $Accelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+    Add-TypeData -Name 'DirectedEdge'
     $Accelerators::Add('DirectedEdge', 'Prelude.DirectedEdge')
 }
 if (-not ('Prelude.Graph' -as [Type])) {
-    Add-Type -Path (Join-Path $PSScriptRoot 'bin/Graph.dll')
-    $Accelerators = [PowerShell].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+    Add-TypeData -Name 'Graph'
     $Accelerators::Add('Graph', 'Prelude.Graph')
 }
 <#
