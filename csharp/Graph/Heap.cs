3ba91729a9d06640dc6d6f9d86e5837272a907dc
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2021-04-18 20:59:58 -0500

    wip(feat): Add empty interface for Heap class

---
+++ csharp/Graph/Heap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prelude {
+    public class Heap {
+        public List<Node> Nodes;
+        public Heap() {
+            Nodes = new List<Node> { };
+        }
+        public static Heap From(IEnumerable<Node> values) {
+            throw new NotImplementedException();
+        }
+        private Heap SiftDown() {
+            throw new NotImplementedException();
+        }
+        private Heap SiftUp() {
+            throw new NotImplementedException();
+        }
+        public Heap Push() {
+            throw new NotImplementedException();
+        }
+        public Node Pop() {
+            throw new NotImplementedException();
+        }
+        public Heap Update(Node value) {
+            throw new NotImplementedException();
+        }
+    }
+}
