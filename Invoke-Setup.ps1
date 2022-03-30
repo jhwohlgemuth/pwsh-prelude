f8b8401e0ef6a61427644e398b82e44009ccfe47
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2022-03-30 16:13:13 -0500

    fix: Remove *improvements* from setup script

--- Invoke-Setup.ps1
+++ Invoke-Setup.ps1
@@ -4,16 +4,9 @@ Param(
     [ValidateSet('windows', 'linux')]
     [String] $Platform = 'windows'
 )
-$Modules = Get-Module -ListAvailable | Select-Object -ExpandProperty Name
 if ($Platform -eq 'windows') {
     dotnet tool restore
 }
-if (-not $Modules -contains 'PSScriptAnalyzer') {
-    Install-Module -Force -Scope CurrentUser -Name PSScriptAnalyzer
-}
-if (-not $Modules -contains 'BuildHelpers') {
-    Install-Module -Force -Scope CurrentUser -Name BuildHelpers
-}
-if (-not $Modules -contains 'Pester') {
-    Install-Module -Force -Scope CurrentUser -Name Pester -SkipPublisherCheck -RequiredVersion 5.3.1
-}
+Install-Module -Force -Scope CurrentUser -Name PSScriptAnalyzer
+Install-Module -Force -Scope CurrentUser -Name BuildHelpers
+Install-Module -Force -Scope CurrentUser -Name Pester -SkipPublisherCheck -RequiredVersion 5.3.1
