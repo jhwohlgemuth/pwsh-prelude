355be0c6025622e0c4f05862285596c1405de96b
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-07-10 20:30:19 -0400

    wip(feat): Add highlight style to selection and menu

--- csharp/CommandLineInterface/CommandLineInterface.cs
+++ csharp/CommandLineInterface/CommandLineInterface.cs
@@ -8,30 +8,34 @@ namespace Prelude {
     using System.Collections.Generic;
 
     public class CommandLineInterface {
+#nullable enable
         public CommandLineInterface() { }
 
+        public const int COLOR_BLUE = 12;
         public const string DEFAULT_MORE_CHOICES_TEXT = "[grey](Move up and down to reveal more items)[/]";
 
         public static Style MenuStyle(Color? foreground = null, Color? background = null, Decoration? decoration = null, string link = "") {
             return new Style(foreground, background, decoration, link);
         }
 
-        public static List<string> Menu(string[] values, int limit = 10, string instructions = "[grey](Press [blue]<space>[/] to toggle, [green]<enter>[/] to accept)[/]") {
+        public static List<string> Menu(string[] values, int limit = 10, Style? style = null, string instructions = "[grey](Press [blue]<space>[/] to toggle, [green]<enter>[/] to accept)[/]") {
             var moreChoicesText = values.Length > limit ? DEFAULT_MORE_CHOICES_TEXT : "";
             var items = AnsiConsole.Prompt(
                 new MultiSelectionPrompt<string>()
                     .NotRequired()
                     .PageSize(limit)
+                    .HighlightStyle(style != null ? style : MenuStyle(COLOR_BLUE))
                     .MoreChoicesText(moreChoicesText)
                     .InstructionsText(instructions)
                     .AddChoices(values));
             return items;
         }
-        public static string Select(string[] values, int limit = 10) {
+        public static string Select(string[] values, int limit = 10, Style? style = null) {
             var moreChoicesText = values.Length > limit ? DEFAULT_MORE_CHOICES_TEXT : "";
             var item = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
                     .PageSize(limit)
+                    .HighlightStyle(style != null ? style : MenuStyle(COLOR_BLUE))
                     .MoreChoicesText(moreChoicesText)
                     .AddChoices(values));
             return item;
